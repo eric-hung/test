82224c4f7f55e9325d03e9e1fcba8af44bc3f09b
hsuyen <hsuyen.hung@gmail.com>
2018-08-16 16:34:19 +0800

    1st commit.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=20180816-Web-Deploy.pubxml -p:Password=GSnc1JcGk5tB2sMQkEnKN7rjArx2SWrakfNd8ySrfhJadvp4tlvvRssvWPl9
+
